e4b10f4d46d5a3a9f62257cdd3da857a57075a24
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2018-12-27 21:40:08 +0700

    IBAction and data models

--- Swoosh-iOS/Controller/LeagueVC.swift
+++ Swoosh-iOS/Controller/LeagueVC.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    @IBOutlet weak var NextButton: BorderButton!
+    var player: Player!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
     
+
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        //player.desiredLeague = "men"
+        //NextButton.isEnabled = true
+        
+        selectedLeague(leagueType: "men")
+    }
+    
+    @IBAction func onWomenTapped(_ sender: Any) {
+        //player.desiredLeague = "women"
+        selectedLeague(leagueType: "women")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        //player.desiredLeague = "coed"
+        selectedLeague(leagueType: "coed")
+    }
+    
+    func selectedLeague (leagueType: String) {
+        player.desiredLeague = leagueType
+        NextButton.isEnabled = true
+    }
 }
---
+++ Swoosh-iOS/Model/PlayerModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerModel.swift
+//  Swoosh-iOS
+//
+//  Created by Tiara Mahardika on 27/12/18.
+//  Copyright © 2018 Tiara Mahardika. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedPlayer: String?
+}
