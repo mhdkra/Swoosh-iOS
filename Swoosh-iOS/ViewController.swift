363204fd08e629695847ba6f97d34747ec5d77ce
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2018-12-27 12:37:33 +0700

    init commit

---
+++ Swoosh-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swoosh-iOS
+//
+//  Created by Tiara Mahardika on 27/12/18.
+//  Copyright © 2018 Tiara Mahardika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
